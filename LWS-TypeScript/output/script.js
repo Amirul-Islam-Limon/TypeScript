9d5b755765295aa1609772c9a1df2a4796f14c3a
Amirul-Islam-Limon <bdamirul0@gmail.com>
2023-11-30 21:15:41 +0600

    TypeScript learning has started YAY!

---
+++ LWS-TypeScript/output/script.js
@@ -0,0 +1,7 @@
+"use strict";
+const countryName = "I love Bangladesh";
+console.log(countryName);
+const myFunc = () => {
+    return console.log("Hi, I'm a arrow function");
+};
+myFunc();
---
+++ LWS-TypeScript/src/script.ts
@@ -0,0 +1,7 @@
+const countryName = "I love Bangladesh";
+console.log(countryName);
+
+const myFunc=()=>{
+    return console.log("Hi, I'm a arrow function");
+}
+myFunc();
